3db2dbd13baacb0bd2216ab94c6de66cf90e067c
initiuminc1 <renjith@renjith-mac.local>
2016-11-30 01:12:36 +0530

    initial commit

--- write.py
+++ write.py
@@ -2,5 +2,6 @@ import happybase
 connection = happybase.Connection('hadoop-master')
 table = connection.table('sayone_test')
 table.put('row-key', {'o:qual1': 'Renjith', 'o:qual2': 'Raj'})
+table.put('row-key-2', {'o:qual1': 'Sayone', 'o:qual2': 'Test'})
 for k, data in table.scan():
   print k, data
