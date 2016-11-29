232931a55f7ea8daa19783aada8e339a7ca13ece
initiuminc1 <renjith@renjith-mac.local>
2016-11-29 23:36:10 +0530

    initial commit

--- sample.py
+++ sample.py
@@ -1,6 +1,9 @@
 import happybase
 
+#First, make sure the thrift server is running:hbase thrift start
+
 connection = happybase.Connection('hadoop-master')
+print(connection.tables())
 table = connection.table('emp')
 
 table.put('1',{'personal': 'Renjith'},{'professional': 'Renjith'})
