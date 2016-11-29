570fa55f7363bf81211247ad838cffe72548e154
initiuminc1 <renjith@renjith-mac.local>
2016-11-30 00:50:46 +0530

    initial commit

--- sample.py
+++ sample.py
@@ -1,10 +1,15 @@
+# -*- coding: utf-8 -*-
+
 import happybase
 
 #First, make sure the thrift server is running:hbase thrift start
 
-connection = happybase.Connection('hadoop-master')
-print(connection.tables())
-table = connection.table('user')
 
-table.put('1',{'first_name': 'Renjith','last_name': 'Raj'})
-print(table.row('1')) # {'f1:': 'hello'}
+connection = happybase.Connection('hadoop-master')
+
+connection.create_table(
+    'sayone_test',
+    {
+     'o': dict(),  # use defaults
+    }
+)
